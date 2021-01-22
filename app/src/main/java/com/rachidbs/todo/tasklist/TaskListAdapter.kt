f75f12db8682102798bffd65e36af2d62b0972cf
Rachid Ben said <rachidbensaid01@gmail.com>
2021-01-22 15:15:04 +0100

    Replace synthetics with ViewBinding

--- app/src/main/java/com/rachidbs/todo/tasklist/TaskListAdapter.kt
+++ app/src/main/java/com/rachidbs/todo/tasklist/TaskListAdapter.kt
@@ -1,30 +1,29 @@
 package com.rachidbs.todo.tasklist
-
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.rachidbs.todo.R
-import kotlinx.android.synthetic.main.item_task.view.*
+import com.rachidbs.todo.databinding.ItemTaskBinding
 
 class TaskListAdapter :
     ListAdapter<Task, TaskListAdapter.TaskViewHolder>(TasksDiffCallback) {
 
-    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class TaskViewHolder(private val binding: ItemTaskBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(task: Task) {
-            itemView.apply {
-                task_title.text = task.title
-                task_description.text = task.description
-            }
+            binding.taskTitle.text = task.title
+            binding.taskDescription.text = task.description
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
-        return TaskViewHolder(itemView)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TaskListAdapter.TaskViewHolder {
+        val binding =
+            ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TaskViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
--- app/src/main/java/com/rachidbs/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/rachidbs/todo/tasklist/TaskListFragment.kt
@@ -1,15 +1,12 @@
 package com.rachidbs.todo.tasklist
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.rachidbs.todo.R
-import kotlinx.android.synthetic.main.fragment_task_list.*
+import com.rachidbs.todo.databinding.FragmentTaskListBinding
 import java.util.*
-
 
 class TaskListFragment : Fragment() {
     private val taskList = mutableListOf(
@@ -17,28 +14,30 @@ class TaskListFragment : Fragment() {
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
     )
+    private lateinit var binding: FragmentTaskListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_task_list, container, false)
+        binding = FragmentTaskListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val taskListAdapter = TaskListAdapter()
-        recycler_view.layoutManager = LinearLayoutManager(activity)
-        recycler_view.adapter = taskListAdapter
-        taskListAdapter.submitList(taskList)
-        floating_button.setOnClickListener {
+        binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+        binding.recyclerView.adapter = taskListAdapter
+        taskListAdapter.submitList(taskList.toList())
+        binding.floatingButton.setOnClickListener {
             taskList.add(
                 Task(
                     id = UUID.randomUUID().toString(),
                     title = "Task ${taskList.size + 1}"
                 )
             )
-            taskListAdapter.submitList(taskList)
+            taskListAdapter.submitList(taskList.toList())
         }
     }
 }
